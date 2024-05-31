476a0356d9cc7d7efe48232cb8972e3d96ba8725
yagnik0 <yashyagnik456@.com>
2024-05-31 20:28:35 +0530

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
- var swiper = new Swiper(".mySwiper", {
-      navigation: {
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-    });
+    },
+});
+
+const nextSlide = document.querySelector(".swiper-button-next");
+const prevSlide = document.querySelector(".swiper-button-prev");
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        swiper.slidePrev()
+    } else if (e.key === "ArrowRight") {
+        swiper.slideNext()
+    }
+});
